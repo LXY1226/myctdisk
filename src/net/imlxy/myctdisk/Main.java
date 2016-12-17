fd605851f623fc89041a26272af6d188e0717a8b
LXY <LXY1226@users.noreply.github.com>
2016-12-17 16:25:09 +0800

    Update Main.java

--- src/net/imlxy/myctdisk/Main.java
+++ src/net/imlxy/myctdisk/Main.java
@@ -1,6 +1,5 @@
 package net.imlxy.myctdisk;
 
-import java.net.URL;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -8,44 +7,54 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
-import java.util.*;
+import java.net.URL;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.*;
+import org.jsoup.nodes.Document;
 
 public class Main {
 	public static void main(String[] args) {
 		System.setProperty("http.agent",
 				"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2725.0 Mobile Safari/537.36");
 		Scanner scann = new Scanner(System.in);
-		System.out.print("下载地址:");
+		System.out.print("URL:");
 		String downuri = scann.nextLine();
 		scann.close();
-		String downurl_final = null;
-		String filename = null;
+		Thread downThread = new Thread(new downthread(parse_file(downuri)));
+		downThread.start();
+	}
+
+	public static String[] parse_file(String file_url) {
+		String[] info = new String[2];
+		// [0] = final url
+		// [1] = filename
 		try {
-			URL url = new URL(downuri);
+			URL url = new URL(file_url);
 			HttpURLConnection urlconn = (HttpURLConnection) url.openConnection();
 			urlconn.setRequestMethod("GET");
 			urlconn.connect();
-			BufferedReader in = new BufferedReader(new InputStreamReader(urlconn.getInputStream(),"utf-8"));
+			BufferedReader in = new BufferedReader(new InputStreamReader(urlconn.getInputStream(), "utf-8"));
 			String line;
 			String result = in.readLine();
 			while ((line = in.readLine()) != null) {
 				result += line + "\n";
 			}
 			Document doc = Jsoup.parse(result);
-			filename = doc.select("div.span10").first().child(0).text();
+			info[1] = doc.select("div.span10").first().child(0).text();
 			String fileinfo = doc.select("div.header_content").first().text();
-			String filesize = fileinfo.substring(fileinfo.indexOf("大小：") + 6, fileinfo.indexOf("B") + 1);
+			String filesize = fileinfo.substring(fileinfo.indexOf("大小：") + 3, fileinfo.indexOf("B") + 1);
 			String randcode = doc.select("#randcode").first().toString();
 			randcode = randcode.substring(randcode.indexOf("value=") + 7, randcode.indexOf(">") - 1);
 			String fileid = doc.select("#file_id").first().toString();
 			fileid = fileid.substring(fileid.indexOf("value=") + 7, fileid.indexOf(">") - 1);
-			System.out.println("文件名:\t\t"+filename);
-			System.out.println("文件大小:\t"+filesize);
-			System.out.println("文件ID:\t\t"+fileid);
-			System.out.println("随机码:\t\t"+randcode);
+			System.out.println("Filename: " + info[1]);
+			System.out.println("Size: " + filesize);
+			// System.out.println("ID: " + fileid);
+			// System.out.println("Randcode:\t\t" + randcode);
 			URL downurl = new URL("http://ctfile.com/guest_loginV2.php");
 			HttpURLConnection urlconn1 = (HttpURLConnection) downurl.openConnection();
 			urlconn1.setRequestMethod("POST");
@@ -63,41 +72,58 @@ public class Main {
 				result1 += line1 + "\n";
 			}
 			String fileinfo1 = result1.substring(result1.indexOf("free_down_action") + 18);
-			downurl_final = fileinfo1.substring(0, fileinfo1.indexOf(",") - 1);
-
+			info[0] = fileinfo1.substring(0, fileinfo1.indexOf(",") - 1);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		File file = new File(filename);
-		System.out.println(file.getAbsolutePath());
+		return info;
+	}
+}
+
+class downthread implements Runnable {
+	private BlockingQueue<String[]> pool = new ArrayBlockingQueue<String[]>(256);
+
+	public downthread(String[] file_info) {
 		try {
-			URL url = new URL(downurl_final);
-			HttpURLConnection urlconn = (HttpURLConnection) url.openConnection();
-			if (file.exists()){
-				System.out.println("已下载："+file.length());
-				urlconn.setRequestProperty("RANGE","bytes="+file.length()+"-");
-			}else{
-				file.createNewFile();
+			pool.put(file_info);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			String[] key = pool.take();
+			HttpURLConnection urlconn = (HttpURLConnection) new URL(key[0]).openConnection();
+			File outfile = new File(key[1]);
+			if (outfile.exists()) {
+				System.out.println("Downloaded：" + outfile.length());
+				urlconn.setRequestProperty("RANGE", "bytes=" + outfile.length() + "-");
+			} else {
+				outfile.createNewFile();
 			}
+
 			urlconn.connect();
 			InputStream in = urlconn.getInputStream();
-			RandomAccessFile oSavedFile = new RandomAccessFile(filename,"rw"); 
-			// 定位文件指针到 nPos 位置 
-			oSavedFile.seek(file.length()); 
-			byte[] b = new byte[524288]; 
-			int nRead; 
-			// 从输入流中读入字节流，然后写到文件中 
-			while((nRead=in.read(b,0,524288)) > 0) 
-			{ 
-			oSavedFile.write(b,0,nRead); 
+			RandomAccessFile oSavedFile = new RandomAccessFile(outfile.getName(), "rw");
+			oSavedFile.seek(outfile.length());
+			byte[] b = new byte[524288];
+			int nRead;
+			long oldtime = new Date().getTime();
+			long oldlength = outfile.length();
+			while ((nRead = in.read(b, 0, 524288)) > 0) {
+				oSavedFile.write(b, 0, nRead);
+				if (new Date().getTime() - oldtime >= 1000) {
+					Date dat = new Date();
+					System.out.print("Speed:" + (oldlength - outfile.length()) / ((oldtime - dat.getTime())) + "kb/s\r");
+					oldlength = outfile.length();
+					oldtime = dat.getTime();
+				}
 			}
 			oSavedFile.close();
-		}
-
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			
 		}
 	}
 }
